1210551c4f613a814e9c159d0b3a4edf90a55d21
Kent <kentqlee@gmail.com>
2016-01-23 15:47:59 -0800

    Solved 01

--- 01_First_Reverse.js
+++ 01_First_Reverse.js
@@ -2,9 +2,11 @@
 
 // Using the JavaScript language, have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order.
 
+// Using a for-loop
 function FirstReverse(str) { 
-
-  // code goes here  
-  return str; 
-         
+  var revStr = "";
+  for (var i = str.length-1; i >= 0; i--) {
+  	revStr += str[i];
+  }
+  return revStr;          
 }
