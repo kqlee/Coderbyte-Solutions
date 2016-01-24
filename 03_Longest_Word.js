2260be14bd56d7bf80eeb35373d0697fe61a9166
Kent <kentqlee@gmail.com>
2016-01-23 16:08:11 -0800

    Complete

---
+++ 03_Longest_Word.js
@@ -0,0 +1,20 @@
+// Longest Word
+
+// Using the JavaScript language, have the function LongestWord(sen) take the sen parameter being passed and return the largest word in the string. If there are two or more words that are the same length, return the first word from the string with that length. Ignore punctuation and assume sen will not be empty. 
+
+// for-loop
+function LongestWord(sen) {
+	var replacement = sen.replace(/[.,-\/#!$%\^&\*;:{}=\-_`~()]/g,''), array = replacement.split(" "), result = '';
+	for (var i = 0; i < array.length; i++) {
+		if (array[i].length > result.length) {
+			result = array[i];
+		} 
+   } return result;
+}
+   
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+LongestWord(readline());
+
+// Input = "fun&!! time" Output = "time"
+// Input = "I love dogs" Output = "love"
---
+++ 05_Simple_Adding.js
@@ -0,0 +1,22 @@
+// Simple Adding
+
+// Using the JavaScript language, have the function SimpleAdding(num) add up all the numbers from 1 to num. For the test cases, the parameter num will be any number from 1 to 1000. 
+
+// for-loop
+function SimpleAdding(num) {
+  newNum = num;
+	for (var i = 1; i < num; i++) {
+	  if (num > 1000) {
+	  	return "error";
+	    } else {
+	    	newNum += i;
+	    }
+	} return newNum;
+}
+   
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+SimpleAdding(readline());                            
+
+// Input = 12 Output = 78
+// Input = 140 Output = 9870
